94aa42bfefd73809ec51e8f12a8dedc13f958418
Christos Georgiou <xris.georgiou@gmail.com>
2016-01-31 18:21:51 +0200

    fix quakes

--- app/quakes/index.js
+++ app/quakes/index.js
@@ -2,30 +2,48 @@ var moment = require('moment');
 var request = require('request');
 var fs = require('fs');
 var express = require('express');
+var _ = require('underscore');
 var router = express.Router();
 
+var _defaults = {
+  datespan: "last_24_hours",
+  limit: 200,
+  mag_min: 3.8,
+};
+
+var _datespans = {
+  last_24_hours: {
+    alias: "last_24_hours",
+    label: "Last 24 hours",
+    from: moment().subtract(24, 'hours').toISOString(),
+    to: moment().toISOString(),
+  },
+  last_15_days: {
+    alias: "last_15_days",
+    label: "Last 15 days",
+    from: moment().subtract(15, 'days').toISOString(),
+    to: moment().toISOString(),
+  },
+  last_month: {
+    alias: "last_month",
+    label: "Last month",
+    from: moment().subtract(1, 'month').toISOString(),
+    to: moment().toISOString(),
+  }
+};
+
 router.get('/data', function(req, res, next) {
-
-  var _ds_from = moment().subtract(24, 'hours').toISOString();
-  var _ds_to = moment().toISOString();
-  var _limit = 200;
-  var _mag_min = 3.8;
-
+  console.log(req.query);
   request.get({
     url: "http://www.seismicportal.eu/fdsnws/event/1/query",
-    formData: {
-      starttime: req.query.ds_from || _ds_from,
-      endtime: req.query.ds_to || _ds_to,
-      limit: req.query.limit || _limit,
-      // lat: req.query.latitude, //DEFAULT ATHENS
-      // lon: req.query.longitude, //DEFAULT ATHENS
-      // minradius: 0,
-      // maxradius: 8,
+    qs: {
+      starttime: _datespans[req.query.ds || _defaults.datespan].from,
+      endtime: _datespans[req.query.ds || _defaults.datespan].to,
+      limit: req.query.lm || _defaults.limit,
       format: "json",
       magtype: "ml",
       nodata: 404,
-      minmag: req.query.mag_min || _mag_min,
-      callback: "JSON_CALLBACK"
+      minmag: req.query.mg || _defaults.mag_min,
     },
   }, function(err, response, data) {
     if (err) throw err;
@@ -40,36 +58,11 @@ router.get('/', function(req, res, next) {
   var ua = require('useragent').parse(_ua);
 
   res.locals.tool = req.app.locals.TOOLS.quakes;
-
-  res.locals.datespan = [{
-    label: "Last 24 hours",
-    from: moment().subtract(24, 'hours').toISOString(),
-    to: moment().toISOString(),
-  }, {
-    label: "Last 15 days",
-    from: moment().subtract(15, 'days').toISOString(),
-    to: moment().toISOString(),
-  }, {
-    label: "Last month",
-    from: moment().subtract(1, 'month').toISOString(),
-    to: moment().toISOString(),
-  }];
-
-  res.locals.filters = {
-    limit: 200,
-    ds: res.locals.datespan[0],
-    mag_min: 3.8,
-  };
-
-  res.locals.leaflet = {
-    center: {
-      lat: 0,
-      lng: 0,
-      zoom: 6
-    },
-    paths: {},
-    markers: {},
-  };
+  res.locals.defaults = _defaults;
+  res.locals.datespans = _.map(_datespans, function(ds) {
+    ds.selected = (ds.alias == req.query.ds) ? "selected" : "";
+    return ds;
+  });
 
 
   res.render('quakes/view', {
--- app/quakes/script.js
+++ app/quakes/script.js
@@ -1,75 +1,62 @@
   $(function() {
 
-    $("#screenWidth").text(screen.width);
-    $("#screenHeight").text(screen.height);
+    var map = L.map('map').setView([0, 0], 1);
+    var mg_colors = ['#008000', '#547c00', '#807500', '#a46a00', '#c05e00', '#df4600', '#ff0000'];
+    var dataLayer = null;
 
-    $("#innerWidth").text(window.innerWidth);
-    $("#innerHeight").text(window.innerHeight);
+    function _loadQuakes() {
 
-    if (navigator.cookieEnabled) {
-      $("#hasCookies").show();
-    } else {
-      $("#hasNotCookies").show();
+      _getQuakes(function(quakes) {
+
+        if (dataLayer) map.removeLayer(dataLayer);
+        dataLayer = new L.FeatureGroup();
+        _.each(quakes.features, function(feature) {
+          L.marker(
+              [feature.properties.lat, feature.properties.lon], {
+                opacity: 0.8,
+              })
+            .bindPopup(feature.properties.flynn_region + " " + feature.properties.time + "<hr style='margin:2px'/> Magnitude: " +
+              feature.properties.mag + " " + feature.properties.magtype + " - Depth: " + feature.properties.depth)
+            .addTo(dataLayer);
+
+          L.circle(
+            [feature.properties.lat, feature.properties.lon],
+            10000 + Math.pow(10, feature.properties.mag), {
+              color: mg_colors[Math.round((feature.properties.mag - 3.5) * 2)],
+              stroke: false,
+              clickable: false,
+              fillOpacity: 0.5,
+            }).addTo(dataLayer);
+
+        });
+        map.addLayer(dataLayer);
+
+      });
+
     }
 
-    var flash = swfobject.getFlashPlayerVersion();
-    if (flash) {
-      $("#hasFlash").show();
-      $("#flashMajor").text(flash.major);
-      $("#flashMinor").text(flash.minor);
-      $("#flashRelease").text(flash.release);
-    } else {
-      $("#hasNotFlash").show();
+    function _getQuakes(cb) {
+      $.getJSON('quakes/data', {
+        ds: $("#ds").val(),
+        lm: $("#lm").val(),
+        mg: $("#mg").val(),
+      }, function(data) {
+        cb(data);
+      });
     }
 
-    $.getJSON('pcdetails/location', function(data) {
-
-      $("#location_ip").text(data.ip);
-      $("#location_city").text(data.city);
-      $("#location_region_name").text(data.region_name);
-      $("#location_country_name").text(data.country_name);
-      $("#location_latitude").text(data.latitude);
-      $("#location_longitude").text(data.longitude);
-      $("#location_time_zone").text(data.time_zone);
-
-      var map = L.map('map').setView([data.latitude, data.longitude], 11);
-
+    function _loadMap() {
       L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
       }).addTo(map);
+    }
 
-      L.marker([data.latitude, data.longitude]).addTo(map);
+    _loadMap();
+    _loadQuakes();
 
-      function _buildQuakes() {
-        vm.leaflet.markers = _.map(vm.quakes.features, function(feature) {
-          return {
-            // group: 'vm.quakes',
-            message: feature.properties.flynn_region + " " + feature.properties.time + "<hr style='margin:2px'/> Magnitude: " +
-              feature.properties.mag + " " + feature.properties.magtype + " - Depth: " + feature.properties.depth,
-            lat: feature.properties.lat,
-            lng: feature.properties.lon,
-            opacity: 0.8
-          };
-        });
-        var _c = ['#008000', '#547c00', '#807500', '#a46a00', '#c05e00', '#df4600', '#ff0000'];
-
-        vm.leaflet.paths = _.map(vm.quakes.features, function(feature) {
-          return {
-            type: "circle",
-            color: _c[Math.round((feature.properties.mag - 3.5) * 2)],
-            stroke: false,
-            clickable: false,
-            fillOpacity: 0.5,
-            radius: Math.pow(10, feature.properties.mag),
-            latlngs: {
-              lat: feature.properties.lat,
-              lng: feature.properties.lon
-            }
-          };
-        });
-      }
-      
+    $("#refresh").on("click", function() {
+      _loadQuakes();
     });
 
   });
